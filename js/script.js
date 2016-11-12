4aafdf07cc44aab5b4991b54828a26a2b3509ca2
Vince Campanale <vcampanale0294@gmail.com>
2016-11-12 17:47:20 -0500

    API call working, able to load display names into individual divs

--- js/script.js
+++ js/script.js
@@ -1,40 +1,45 @@
-//initialize request object
-var xhr = new XMLHttpRequest();
-
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var usernames = [];
 
 users.forEach(function(user){
-  var endpoint = makeURL(user);
-  //construct get request to desired URL, set asynchronous to false since iterating over an array
-  xhr.open('GET', endpoint, false);
-  //send the request
-  xhr.send();
-  //listen for the readystatechange event on our xhr object and call the processRequest event handler when the event gets overheard
-  xhr.onreadystatechange = processRequest;
-  function processRequest(e){
-    //check if request is completed (readyState == 4) and is successful (status == 200)
-    if(xhr.readyState == 4 && xhr.status == 200){
-      //actual logic of request
-      var response = JSON.parse(xhr.responseText);
-      usernames.push(response.display_name);
-    }
-  }
+  loadData(makeUrl(user), 10000, processData);
 });
 
-alert(usernames);
 
-//separate logic for endpoint builder function
-function makeURL(user){
+//process data callback
+function processData(response){
+  var obj = JSON.parse(this.response);
+  var new_div = document.createElement('div');
+  new_div.className = "channel";
+  new_div.innerHTML = '<h3>' + obj.display_name + '</h3>';
+
+  document.getElementById('channel__list').appendChild(new_div);
+  console.log(obj);
+}
+
+//build user endpoint
+function makeUrl(user){
   var base = 'https://crossorigin.me/https://wind-bow.hyperdev.space/twitch-api';
   var userPart = '/users/' + user;
   var API = base + userPart;
   return API;
 }
 
-function loadData(sUrl, timeout, callback){
-
-  var args = arguments.slice(3);
+//make get request to endpoint
+function loadData(url, timeout, callback){
   var xhr = new XMLHttpRequest();
-
+  xhr.ontimeout = function() {
+    console.error("The request for " + url + " timed out.");
+  };
+  xhr.onload = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback.apply(xhr, xhr);
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.timeout = timeout;
+  xhr.send(null);
 }
