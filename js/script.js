cec296210e743f5d2f60d9079b979c849c320e02
Vince Campanale <vcampanale0294@gmail.com>
2016-11-15 15:26:22 -0500

    fully functional, except for little circles next to channel names

--- js/script.js
+++ js/script.js
@@ -13,37 +13,50 @@ function loadPage(){
       }).done(function(data){
         var channel_div = document.createElement('div');
         channel_div.className = "channel";
-
         if(data.stream === null){
-          channelData.push("Channel is offline");
-          channel_div.className += " offline";
+          channelData.push("Channel is offline"); //channelData[2] contains "Channel is offline"
+          channel_div.className += " offline little-offline-circle";
         }
         if(data.stream !== null){
-          channelData.push(data.stream.channel.status);
-          channel_div.className += " online";
+          var status = data.stream.channel.status;
+          if(status.length > 40){
+            var trimmed = status.substring(0, 40) + '...';
+            channelData.push(trimmed); //channelData[2] contains status of channel
+          } else {
+            channelData.push(status);
+          }
+          channel_div.className += " online little-online-circle";
         }
-
-
-
         channel_div.innerHTML = `
-            <h3><a href=` + channelData[1] + `>` + channelData[0] + `</a></h3>
+            <h3><a href=` + channelData[1] + `>` + channelData[0] + `</a></h3>\n
+            <p>` + channelData[2] + `</p>
           `;
-
-
         document.getElementById('channel__list').appendChild(channel_div);
-
-
-
-        //TODO: Add code to put channelData into DOM elements.
-
-
-
-        console.log(channelData);
       });
-
-
-
     });
+  });
+
+
+  $(".online-circle").click(function(){
+    $(".offline").addClass('hide');
+    if($(".online").hasClass('hide')){
+      $(".online").removeClass('hide');
+    }
+  });
+
+  $(".offline-circle").click(function(){
+    $(".online").addClass('hide');
+    if($(".offline").hasClass('hide')){
+      $(".offline").removeClass('hide');
+    }
+  });
+  $(".all-circle").click(function(){
+    if($(".online").hasClass('hide')){
+      $(".online").removeClass('hide');
+    }
+    if($(".offline").hasClass('hide')){
+      $(".offline").removeClass('hide');
+    }
   });
 
   //build endpoint
